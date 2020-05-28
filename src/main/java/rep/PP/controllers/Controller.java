91e09a41016722c00733a75d5b475ac65d5fd733
madalin11 <madalin.c.puiu@gmail.com>
2020-05-28 22:25:37 +0300

    GRA-30// Adaugare functionalitate animatie din scena principala.

--- src/main/java/rep/PP/controllers/Controller.java
+++ src/main/java/rep/PP/controllers/Controller.java
@@ -1,12 +1,15 @@
 package rep.PP.controllers;
 
 import animatefx.animation.*;
+import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,11 +21,34 @@ public class Controller implements Initializable {
     @FXML
     private BorderPane borderPane;
 
-    public void initialize(URL location, ResourceBundle resources) {
+    @FXML
+    private Circle c1;
 
+    @FXML
+    private Circle c2;
+
+    @FXML
+    private Circle c3;
+
+    public void initialize(URL location, ResourceBundle resources) {
+    setRotate(c1,true,180,10);
+    setRotate(c2,true,360,18);
+    setRotate(c3,true,360,5);
     }
     double x = 0;
     double y = 0;
+
+    private void setRotate(Circle c1 , boolean reverse , int angle , int duration){
+        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(duration),c1);
+
+        rotateTransition.setAutoReverse(reverse);
+        rotateTransition.setByAngle(angle);
+        rotateTransition.setDelay(Duration.seconds(0));
+        rotateTransition.setRate(3);
+        rotateTransition.setCycleCount(200);
+        rotateTransition.play();
+    }
+
     @FXML
     void close(javafx.scene.input.MouseEvent event) {
         Stage stage=(Stage) ((Node)event.getSource()).getScene().getWindow();
