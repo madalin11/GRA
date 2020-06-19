2de0276777f04d25ecb727e95c93b5cfc4befcc3
madalin11 <madalin.c.puiu@gmail.com>
2020-06-19 22:52:51 +0300

    Adaugare clasa FileSystemService    // GRA-25

--- src/main/java/rep/PP/services/FileSystemService.java
+++ src/main/java/rep/PP/services/FileSystemService.java
@@ -4,7 +4,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileSystemService {
-    private static final String APPLICATION_FOLDER = ".Project_FIS20-PP";
+    private static final String APPLICATION_FOLDER = ".ANA-Gherburai";
     private static final String USER_FOLDER = System.getProperty("user.home");
     public static final Path APPLICATION_HOME_PATH = Paths.get(USER_FOLDER, APPLICATION_FOLDER);
 
