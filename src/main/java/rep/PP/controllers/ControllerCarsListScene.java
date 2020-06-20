69ec747832582bd2415ba49cf757afd0e89092f3
madalin11 <madalin.c.puiu@gmail.com>
2020-06-20 16:52:00 +0300

    Creare controler car list impreuna cu cautare masini din fisier json // GRA-19

--- src/main/java/rep/PP/controllers/ControllerCarsListScene.java
+++ src/main/java/rep/PP/controllers/ControllerCarsListScene.java
@@ -1,9 +1,20 @@
 package rep.PP.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,8 +23,45 @@ public class ControllerCarsListScene implements Initializable {
     @FXML
     private VBox carsListScene;
 
-    @Override
+    @FXML
+    private ListView<String> carListview;
+
+    @FXML
+    private HBox hboxPane;
+
+    @FXML
+    void goConfigScene(ActionEvent event) {
+
+    }
+
+
+    private JSONArray rooms = new JSONArray();
     public void initialize(URL location, ResourceBundle resources) {
+
+        Object p;
+        JSONParser parser = new JSONParser();
+        try {
+            FileReader readFile = new FileReader("src/main/java/rep/PP/data/employees.json");
+            BufferedReader read = new BufferedReader(readFile);
+            p = parser.parse(read);
+            if (p instanceof JSONArray) {
+                rooms = (JSONArray) p;
+            }
+
+        } catch (ParseException | IOException ex) {
+            ex.printStackTrace();
+        }
+
+        for (int i=0;i<rooms.size();i++) {
+
+            JSONObject obj=(JSONObject) rooms.get(i);
+
+            carListview.getItems().add("         " + obj.get("name").toString() );
+        }
+        //listView.getItems().addAll(rooms);
+        carListview.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+
 
     }
 }
